a0adad778415416203e4b4facaa12cfe0cca86ab
Lechuck Roh <member@lechuck2011.cafe24.com>
2019-07-24 21:11:24 +0900

    bump version to v1.0.3

--- main.go
+++ main.go
@@ -119,7 +119,7 @@ func generate(c *cli.Context) error {
 func main() {
 	cliApp := cli.NewApp()
 	cliApp.Name = "oct"
-	cliApp.Version = "0.0.1"
+	cliApp.Version = "1.0.3"
 	cliApp.Compiled = time.Now()
 	cliApp.Authors = []cli.Author{
 		{Name: "Lechuck Roh"},
