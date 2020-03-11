9d6987eb89236eefc9974219241c891ed46ce354
Lechuck Roh <member@lechuck2011.cafe24.com>
2020-03-11 10:16:18 +0900

    1.0.13

--- main.go
+++ main.go
@@ -163,7 +163,8 @@ func generate(c *cli.Context) error {
 	return cmd.Generate(input, output)
 }
 
-const VERSION = "1.0.12"
+const VERSION = "1.0.13"
+
 var buildDateVersion string
 
 func main() {
